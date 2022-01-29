af35792af0c2d0337ec645b7ee6247752c6c458f
PHELIPE STERING <68488889+phelipestering@users.noreply.github.com>
2022-01-29 13:11:58 -0300

    Construindo Layout Albuns

--- public/pages/albums.php
+++ public/pages/albums.php
@@ -1,1 +1,18 @@
-albums
+<h1>Album</h1>
+
+<div class="row">
+    <?php
+        for($i=1; $i<=10; $i++):
+    ?>
+    <div class="col-3 album">
+
+        <a href="">
+            <img src="../albunsCovers/eneeeias56.jpeg" alt="<?=$i?>" class="img-album">
+            <h4>Album <?=$i?> </h4>
+        </a>
+
+    </div>
+    <?php
+        endfor;
+    ?>
+</div>
