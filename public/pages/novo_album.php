17778e0aa91a2904b976f51d058db303224687b0
PHELIPE STERING <68488889+phelipestering@users.noreply.github.com>
2022-01-29 21:50:54 -0300

    Erro no formulario de envio de aquivos

--- public/pages/novo_album.php
+++ public/pages/novo_album.php
@@ -3,17 +3,41 @@
 
 <h1>Cadastrar Novo Album</h1>
 
-<form action="#" method= "POST" class="action">
-
+<form action="" method="post" class="action" enctype="multipart/form-data">
     <div class="form-group">
-        <input type="text" name = "name" placeholder="Nome:"  class="form-control">
+        <input type="text" name = "name" placeholder="Nome:"  class="form-control"/>
     </div>
-
     <div class="form-group">
-        <input type="file" name = "image" placeholder="Nome:"  class="form-control">
+        <input type="file" name="image" class="form-control" >
     </div>
-
     <button type="submit" class="btn btn-success" >Cadastrar</button>
-
 </form>
 
+
+
+<?php
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        
+        $album = $_POST['name'];
+        $path = "Albuns/{$album}";
+
+        if (!is_dir($path)){
+            mkdir($path);
+        }
+
+        $file = $_FILES['image'];
+        $fileInfo = explode('.', $file['name']);
+
+        $extension = $fileInfo[1];
+        $nameImage = $album . '.' . $extension;
+
+        if(move_uploaded_file($file['tmp_name'], $path.'/'.$nameImage )){
+            header('Location: ?page=albums');
+        } else {
+            echo 'Deu pau...';
+        }
+
+    }
+
+?>
